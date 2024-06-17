4c96cafe329ffce749a1ccd4400a89e83a5c7052
AduAtakoraJustice <jadu-atakora@st.ug.edu.gh>
2024-06-17 15:34:57 +0000

    First commit "inheritance"

---
+++ index.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace OOPConcepts
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
