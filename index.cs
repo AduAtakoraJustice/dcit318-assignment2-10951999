39017600b76d99bfd3cdaf2b464596412e3cbd9d
AduAtakoraJustice <jadu-atakora@st.ug.edu.gh>
2024-06-17 15:36:58 +0000

    second commit "abstract classes"

--- index.cs
+++ index.cs
@@ -29,3 +29,44 @@ namespace OOPConcepts
         }
     }
 
+    // Abstract base class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Derived class Circle
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Derived class Rectangle
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+   
